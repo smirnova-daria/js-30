a54c5e1fed8177a2d95ea4e2f034013edcef98a5
Daria Smirnova <darya.smirn1@yandex.ru>
2022-09-17 21:49:16 +0300

    add drum machine

---
+++ drum-kit/index.js
@@ -0,0 +1,19 @@
+const keys = document.querySelectorAll(".key");
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.play();
+  audio.currentTime = 0;
+  key.classList.add("playing");
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+window.addEventListener("keydown", playSound);
+
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
