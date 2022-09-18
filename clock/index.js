5a1f5927d1f81390b4f5007fc052df1152ac1e09
Daria Smirnova <darya.smirn1@yandex.ru>
2022-09-18 22:10:34 +0300

    add js-clock

---
+++ clock/index.js
@@ -0,0 +1,22 @@
+const secondsHand = document.querySelector(".seconds-hand");
+const minutesHand = document.querySelector(".minutes-hand");
+const hoursHand = document.querySelector(".hours-hand");
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  const minutesDegrees = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
+  const hoursDegrees = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
+
+  secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(setDate, 500);
+setDate();
