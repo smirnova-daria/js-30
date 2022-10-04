9652e98eb8ef8dd63c34f6755a36651310ce3aa7
Daria Smirnova <darya.smirn1@yandex.ru>
2022-10-04 15:17:28 +0300

    add multiple checkboxes

---
+++ multiple-checkboxes/index.js
@@ -0,0 +1,32 @@
+const checkboxes = document.querySelectorAll('.item input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e) {
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach((checkbox) => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+  if (e.shiftKey && !this.checked) {
+    checkboxes.forEach((checkbox) => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+      if (inBetween) {
+        checkbox.checked = false;
+      }
+    });
+  }
+  lastChecked = this;
+}
+
+checkboxes.forEach((checkbox) =>
+  checkbox.addEventListener("click", handleCheck)
+);
